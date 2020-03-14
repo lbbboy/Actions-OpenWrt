8b4a63d54d2e25b58089f440110f701737291c82
lbbboy <lbbboy@gmail.com>
2020-03-14 18:20:25 +0800

    .2020-3-14.

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+echo 'theme-material'
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
+
